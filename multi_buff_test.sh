0c47881dc0b1ce07fca8afa72afce53763b18ac2
Nobusuke IWASAKI <wata909@gmail.com>
2016-07-09 20:19:42 +0900

    Update multi_buff_test.sh

--- multi_buff_test.sh
+++ multi_buff_test.sh
@@ -4,7 +4,7 @@ i=1
 
 while [ $i -le 13 ]
  do
- v.extract --overwrite input=point output=point_select where="cat="$i""
+ v.extract --overwrite InvPoint_dragonfly  output=point_select where="cat="$i""
 # v.buffer --overwrite input=point_select output=point_select_buff type=point distance=500
 # v.type --overwrite input=point_select output=point_select_cent type=point,centroid
 # v.patch --overwrite input=point_select_cent,point_select_buff output=point_select_patch
