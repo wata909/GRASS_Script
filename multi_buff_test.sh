52424ac6d509d07a28d93973b4d949930f564c49
Nobusuke IWASAKI <wata909@gmail.com>
2016-07-09 20:40:11 +0900

    Update multi_buff_test.sh

--- multi_buff_test.sh
+++ multi_buff_test.sh
@@ -6,11 +6,11 @@ while [ $i -le 13 ]
  do
  v.extract --overwrite InvPoint_dragonfly  output=point_select where="cat="$i""
  v.buffer --overwrite input=point_select output=point_select_buff type=point distance=500
-# v.type --overwrite input=point_select output=point_select_cent type=point,centroid
+ v.type --overwrite input=point_select output=point_select_cent type=point,centroid
 # v.patch --overwrite input=point_select_cent,point_select_buff output=point_select_patch
 # db.copy from_table=point_select_cent to_table=point_select_patch
 # v.db.connect map=point_select_patch table=point_select_patch
-# v.overlay --overwrite ainput=point_select_patch binput=veg_merge output=veg_overlay_point"$i" operator=and
+# v.overlay --overwrite ainput=point_select_patch binput=landuse_diss output=veg_overlay_point"$i" operator=and
 # v.db.addcol map=veg_overlay_point"$i" columns="area DOUBLE PRECISION"
 # v.to.db map=veg_overlay_point"$i" option=area units=meters columns=area
 # v.out.ogr input=veg_overlay_point"$i" type=area dsn=/C/GIS_DATA olayer=point"$i"_buffer
